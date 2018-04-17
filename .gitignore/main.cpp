d93a998f7ade9aed80cdaf436358c220106ffd4e
GeorgiDg <33101215+Georgidg@users.noreply.github.com>
2018-04-17 18:23:10 +0300

    Create main.cpp

---
+++ .gitignore/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+#include "CircularInt.hpp"
+
+
+
+int main() {
+	CircularInt hour{ 1, 12 };                 // <hour is an integer between 1 and 12, like an hour on the clock>
+	cout << hour << endl;                     // 1
+	cout << endl;
+
+	hour += 4;  cout << hour << endl;         // 5
+	cout << endl;
+
+	(hour += 2)++;  cout << hour << endl;     // 8
+	cout << endl;
+
+	hour += 18;   cout << hour << endl;       // 2   (18 hours after 8)
+	cout << endl;
+
+	cout << -hour << endl;                    // 10  (2 hours before midnight)
+	cout << endl;
+
+	hour = 1 - hour; cout << hour << endl;    // 11  (2 hours before 1)
+	cout << endl;
+
+	cout << hour + hour << endl;                // 10 (11 hours after 11)
+	cout << endl;
+
+	hour *= 2;   cout << hour << endl;        // 10 (11*2 = 11+11)
+	cout << endl;
+
+	cout << hour / 2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
+	cout << endl;
+	try {
+		cout << hour / 3;
+	}
+	catch (const string& message) {
+		cout << message << endl;     // "There is no number x in {1,12} such that x*3=10"
+	}
+
+	// RIDDLES (not for submission): 
+	//  * when is there exactly one answer to a/b?
+	//  * when are there two or more answers to a/b?
+	//	* when is there no answer to a/b?
+}
